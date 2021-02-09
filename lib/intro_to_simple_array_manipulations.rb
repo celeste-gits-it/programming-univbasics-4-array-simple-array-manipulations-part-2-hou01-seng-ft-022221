af663ffea276562703096aa7243a48759a270962
Celeste Carter <celesteanell@gmail.com>
2021-02-09 20:10:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,24 +2,3 @@ def using_concat(array1, array2)
 array1.concat(array2)
 end
 
-def using_insert(list_of_programming_languages,another_language)
- list_of_programming_languages.insert(4,another_language)
-end
-
-
-def using_uniq(haircuts)
-  haircuts.uniq
-end
-
-def using_flatten(instruments)
-  instruments.flatten
-end
-
-
-def using_delete(instructors,no_offense_steven)
-  instructors.delete(no_offense_steven)
-end
-
-def using_delete_at(famous_robots,deleted_robot)
-  famous_robots.delete_at(deleted_robot)
-end
